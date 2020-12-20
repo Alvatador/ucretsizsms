7926d102e6be78c9d3ce905024268023d72f469d
Alvatador <69437783+Alvatador@users.noreply.github.com>
2020-12-20 21:44:53 +0300

    Update smsgönderme.py

--- smsgönderme.py
+++ smsgönderme.py
@@ -8,34 +8,46 @@ import time
 from colorama import Fore, Back, Style
 
 
+print("[ENG] Welcome To Free Sms Tool")
+time.sleep(3)
+
+os.system("clear")
+
+print("[TR] Hoşgeldin Ücretsiz Sms Aracına ")
+time.sleep(3)
+
+os.system("clear")
 
 
-
-banner = print(Fore.YELLOW+"""
+banner = print(Fore.YELLOW+
+"""
     ___    __            __            __          
    /   |  / /   ______ _/ /_____ _____/ /___  _____
   / /| | / / | / / __ `/ __/ __ `/ __  / __ \/ ___/
  / ___ |/ /| |/ / /_/ / /_/ /_/ / /_/ / /_/ / /    
 /_/  |_/_/ |___/\__,_/\__/\__,_/\__,_/\____/_/     
                                                    
-
-| Hergün 1 Tane Mesaj Atma Hakkınız Vardır (Vpnle İstediğiniz Kadar Atabilirsiniz)
-| Mesajınız Karakter Sayısı Sınırlı Bunu Mesajınızı Yazdıktan Sonra Görüceksiniz.
-| Telefon Numarasını Doğru Girmezseniz Hata Alabilirsiniz.
-
+[TR] Hergün 1 Tane Mesaj Atma Hakkınız Vardır.
+[ENG] You Have The Right To Send 1 Message a Day.
+------------------------------------------------------------------------------------
+[TR] Mesajınız Karakter Sayısı Sınırlı Bunu Mesajınızı Yazdıktan Sonra Görüceksiniz.
+[ENG] Your Message is Limited Number of Characters You Will See This After You Write Your Message.
+------------------------------------------------------------------------------------
+[TR] Telefon Numarasını Doğru Girmezseniz Hata Alabilirsiniz.
+[ENG] If You Don't Enter The Phone Number Correctly, You May Get An Error.
 """)
 
 print(banner)
 
-sor = input("Tel adresi Örnek:+905404212233 >>> ")
+sor = input("Telefon Numarası Örnek: +905404212233 >>> ")
 
 mesaj = input("Mesajınız >>> ")
 
 arlk = mesaj[0:70]
 
-print("\n| Mesajınızın Gönderilebilecek kısmı aşagıdaki gibidir.\n"+arlk)
+print("\n| Mesajınızın Gönderilebilecek kısmı aşağıdaki gibidir.\n"+arlk)
 
-drlm = input("\n| Mesajınız Gönderilsinmi?[Y/N] >>> ")
+drlm = input("\n| Mesajınız Gönderilsinmi? [Y/N] >>> ")
 
 if drlm == "y" or drlm == "Y":
     print("\n"+sor+"\n"+arlk+"\n")
@@ -52,9 +64,9 @@ else:
     print("\n|Hata!")
 os.system("clear")
 print("Bizi Tercih Ettiğiniz İçin Teşekkürler")
+time.sleep(1)
 print("Bizi Tercih Ettiğiniz İçin Teşekkürler")
-print("Bizi Tercih Ettiğiniz İçin Teşekkürler")
-print("Bizi Tercih Ettiğiniz İçin Teşekkürler")
+time.sleep(1)
 print("Bizi Tercih Ettiğiniz İçin Teşekkürler")
 time.sleep(1)
 os.system("clear")
@@ -69,23 +81,21 @@ print("Alvatador Ücretsiz Sms Gönderme")
 time.sleep(1)
 os.system("clear")
 print(" ")
-print("İletişim Adresleri")
 banner = print(Fore.RED+"""
-  ______   __                        __                      __                     
- /      \ /  |                      /  |                    /  |                    
-/$$$$$$  |$$ | __     __  ______   _$$ |_     ______    ____$$ |  ______    ______  
-$$ |__$$ |$$ |/  \   /  |/      \ / $$   |   /      \  /    $$ | /      \  /      \ 
-$$    $$ |$$ |$$  \ /$$/ $$$$$$  |$$$$$$/    $$$$$$  |/$$$$$$$ |/$$$$$$  |/$$$$$$  |
-$$$$$$$$ |$$ | $$  /$$/  /    $$ |  $$ | __  /    $$ |$$ |  $$ |$$ |  $$ |$$ |  $$/ 
-$$ |  $$ |$$ |  $$ $$/  /$$$$$$$ |  $$ |/  |/$$$$$$$ |$$ \__$$ |$$ \__$$ |$$ |      
-$$ |  $$ |$$ |   $$$/   $$    $$ |  $$  $$/ $$    $$ |$$    $$ |$$    $$/ $$ |      
-$$/   $$/ $$/     $/     $$$$$$$/    $$$$/   $$$$$$$/  $$$$$$$/  $$$$$$/  $$/                                                                                      
+
+ █████╗ ██╗    ██╗   ██╗ █████╗ ████████╗ █████╗ ██████╗  ██████╗ ██████╗ 
+██╔══██╗██║    ██║   ██║██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗██╔═══██╗██╔══██╗
+███████║██║    ██║   ██║███████║   ██║   ███████║██║  ██║██║   ██║██████╔╝
+██╔══██║██║    ╚██╗ ██╔╝██╔══██║   ██║   ██╔══██║██║  ██║██║   ██║██╔══██╗
+██║  ██║███████╗╚████╔╝ ██║  ██║   ██║   ██║  ██║██████╔╝╚██████╔╝██║  ██║
+╚═╝  ╚═╝╚══════╝ ╚═══╝  ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝
+                                                                                                                                                             
 """)
+time.sleep(1)
+os.system("clear")
 print(banner)
+print("İletişim Adresleri")
 banner = print(Fore.GREEN+"""
-Discord: BoranBeyy#0001
+Discord: Alvatador#1000
+İnstagram: boranpxrd
 """)
-
-
-
-
